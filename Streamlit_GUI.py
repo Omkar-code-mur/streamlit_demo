ad0202455b593f397b142c7dc0ae1b995dfbee3c
Omkar Ambalal Kodmur <omkarkodmur2031@gmail.com>
2023-06-08 13:44:54 +0530

    first

---
+++ Streamlit_GUI.py
@@ -0,0 +1,25 @@
+from streamlit_webrtc import webrtc_streamer
+import av
+import cv2
+
+import streamlit as st
+
+st.slider("Threshold1",0,100)
+st.slider("Threshold2",0,100)
+
+def callback(frame: av.VideoFrame) -> av.VideoFrame:
+    img = frame.to_ndarray(format="bgr24")
+    img = cv2.Canny(img,100,200)
+    img = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+    
+    
+    return av.VideoFrame.from_ndarray(img,format="bgr24")
+webrtc_streamer(
+    # ...
+    rtc_configuration={  # Add this config
+        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+    },
+    # ...
+    key="sample",    
+    video_frame_callback=callback
+    )
